baf53f3f80a81445c24adc531ba0375ba72e2ec1
Matt West <mattwest@google.com>
2024-11-30 23:27:41 -0600

    Add day 1 solution in go.

--- src/day01.go
+++ src/day01.go
@@ -5,22 +5,66 @@ import (
 	"io"
 	"log"
 	"os"
+	"slices"
+	"strconv"
+	"strings"
 )
 
-func parse(s string) string {
-	return s
+func parse(s string) ([]int, []int) {
+	lines := strings.Split(s, "\n")
+	var left []int
+	var right []int
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		parts := strings.Fields(line)
+		l, err := strconv.Atoi(parts[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		left = append(left, l)
+		r, err := strconv.Atoi(parts[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		right = append(right, r)
+	}
+	return left, right
 }
 
 func part1(input string) string {
-	return input
+	left, right := parse(input)
+	slices.Sort(left)
+	slices.Sort(right)
+	var dist int
+	for i, _ := range left {
+		d := left[i] - right[i]
+		if d < 0 {
+			d = -d
+		}
+		dist += d
+	}
+	return fmt.Sprint(dist)
 }
 
 func part2(input string) string {
-	return input
+	left, right := parse(input)
+	similarity := 0
+	for _, l := range left {
+		sim := 0
+		for _, r := range right {
+			if l == r {
+				sim++
+			}
+		}
+		similarity += sim * l
+	}
+	return fmt.Sprint(similarity)
 }
 
 func main() {
-	f, err := os.Open("../data/day01.txt")
+	f, err := os.Open("data/day01.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
--- src/day01_test.go
+++ src/day01_test.go
@@ -1,39 +1,8 @@
 package main
 
-import "testing"
-
-func Test_parse(t *testing.T) {
-	type args struct {
-		s string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{
-			name: "empty string",
-			args: args{
-				s: "",
-			},
-			want: "",
-		},
-		{
-			name: "pass through",
-			args: args{
-				s: "this is a test",
-			},
-			want: "this is a test",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := parse(tt.args.s); got != tt.want {
-				t.Errorf("parse() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+import (
+	"testing"
+)
 
 func Test_part1(t *testing.T) {
 	type args struct {
@@ -45,11 +14,17 @@ func Test_part1(t *testing.T) {
 		want string
 	}{
 		{
-			name: "empty string",
+			name: "test input",
 			args: args{
-				input: "",
+				input: `3   4
+                        4   3
+                        2   5
+                        1   3
+                        3   9
+                        3   3`,
 			},
-			want: ""},
+			want: "11",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -69,28 +44,24 @@ func Test_part2(t *testing.T) {
 		args args
 		want string
 	}{
-		// TODO: Add test cases.
+		{
+			name: "test input",
+			args: args{
+				input: `3   4
+                        4   3
+                        2   5
+                        1   3
+                        3   9
+                        3   3`,
+			},
+			want: "31",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := part2(tt.args.input); got != tt.want {
 				t.Errorf("part2() = %v, want %v", got, tt.want)
 			}
-		})
-	}
-}
-
-func Test_main(t *testing.T) {
-	tests := []struct {
-		name string
-	}{
-		{
-			name: "main",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			main()
 		})
 	}
 }
