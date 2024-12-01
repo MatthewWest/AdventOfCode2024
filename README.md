15dc87ef2085971c0fd9792dd663ee78938cf3b6
Matt West <mattwest@google.com>
2024-11-30 20:43:41 -0600

    Clear 2023 files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# AdventOfCode2023
-My solutions to the Advent of Code challenges from 2023.
+# AdventOfCode2024
+My solutions to the Advent of Code challenges from 2024.
--- src/AdventOfCode2023.jl
+++ src/AdventOfCode2023.jl
@@ -1,10 +1,10 @@
-module AdventOfCode2023
+module AdventOfCode2024
 import Printf.@sprintf
 
-solved = 1:5
+solved = 1:1
 for day ∈ solved
     padded = @sprintf("%02d", day)
     include(joinpath(@__DIR__, "day$padded.jl"))
 end
 
-end # module AdventOfCode2023
+end # module AdventOfCode2024
